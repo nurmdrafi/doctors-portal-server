6fac96ced98adc2ebc7a86eb20009adc69ee74fe
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-14 16:47:37 +0600

    connected mongodb and GET services

--- index.js
+++ index.js
@@ -2,20 +2,47 @@ const express = require("express");
 const app = express(); // create app by calling express()
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
-require('dotenv').config();
+require("dotenv").config();
 
 // use middleware
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yffpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    //   console.log("mongodb connected")
+    const serviceCollection = client.db("doctorDB").collection("services");
+
+    // Get all services
+    // http://localhost:5000/service
+    app.get("/service", async (req, res) => {
+      const query = req.query;
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    //   await client.close();
+  }
+}
+run().catch(console.dir);
 
 // for testing
 app.get("/", (req, res) => {
-    res.send({ message: "Success" });
+  res.send({ message: "Success" });
 });
 
 app.listen(port, () => {
-    console.log("Listening to port", port);
+  console.log("Listening to port", port);
 });
