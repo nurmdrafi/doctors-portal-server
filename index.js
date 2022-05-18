03f3bd531b11a31d4a164730d6146eb08264d35c
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-18 20:25:48 +0600

    implement GET available services by date

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ async function run() {
     await client.connect();
     //   console.log("mongodb connected")
     const serviceCollection = client.db("doctorDB").collection("services");
-    const appointmentCollection = client.db("doctorDB").collection("appointments");
+    const appointmentCollection = client
+      .db("doctorDB")
+      .collection("appointments");
 
     // Get all services
     // http://localhost:5000/service
@@ -34,6 +36,43 @@ async function run() {
       res.send(services);
     });
 
+    // Warning:
+    // This is not the proper way to query
+    // After learning more about mongodb, use aggregate lookup, pipeline, match, group
+    // https://www.mongodb.com/docs/manual/aggregation/
+
+    // Get available services
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      // step 1: get all services [ 6 x {}]
+      const services = await serviceCollection.find().toArray();
+
+      // step 2: get the booking of that day [2 x {}]
+      const query = { date: date };
+      const appointments = await appointmentCollection.find(query).toArray();
+      console.log(appointments)
+      // step 3: for each service
+      services.forEach((service) => {
+        // step 4: filter which services are booked [2 x {}] from all services
+        const serviceAppointments = appointments.filter(
+          (appointment) => appointment.treatment === service.name
+        );
+
+        // step 5: select slots for the service bookings: ["", "", "", "", ""]
+        const bookedSlots = serviceAppointments.map((s) => s.slot);
+
+        // step 6: get available slots (service slots - booked slots)
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        // step 7: set available slots to make it easier
+        service.slots = available;
+        // service.available = available;
+      });
+      res.send(services);
+    });
+
     // Post appointment
     // http://localhost:5000/appointment
     /* 
@@ -41,13 +80,17 @@ async function run() {
     */
     app.post("/appointment", async (req, res) => {
       const appointment = req.body;
-      const query = {treatment: appointment.treatment, date: appointment.date, patientName: appointment.patientName}
-      const exists = await appointmentCollection.findOne(query)
-      if(exists){
-        return res.send({success: false, appointment: exists})
+      const query = {
+        treatment: appointment.treatment,
+        date: appointment.date,
+        patientName: appointment.patientName,
+      };
+      const exists = await appointmentCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, appointment: exists });
       }
       const result = await appointmentCollection.insertOne(appointment);
-      res.send({success: true, result});
+      res.send({ success: true, result });
     });
   } finally {
     //   await client.close();
